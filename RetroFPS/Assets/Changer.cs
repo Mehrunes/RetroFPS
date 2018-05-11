cc1f991910e162dfa6aebae0a7495249bb2aa2ad
unknown <krzcwi@st.amu.edu.pl>
2018-05-11 13:53:43 +0200

    sprajty cz1

---
+++ RetroFPS/Assets/Changer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changer : MonoBehaviour {
+
+    public Sprite spFront, spLeft, spRight, spBack;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+            this.GetComponent<SpriteRenderer>().sprite = spFront;
+        if (Input.GetKey(KeyCode.A))
+            this.GetComponent<SpriteRenderer>().sprite = spLeft;
+        if (Input.GetKey(KeyCode.D))
+            this.GetComponent<SpriteRenderer>().sprite = spRight;
+        if (Input.GetKey(KeyCode.S))
+            this.GetComponent<SpriteRenderer>().sprite = spBack;
+    }
+}
