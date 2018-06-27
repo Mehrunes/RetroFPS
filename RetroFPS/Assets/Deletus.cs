9859c133e5d2f3a60e0737c2536360f84d9f4130
unknown <krzcwi@st.amu.edu.pl>
2018-06-27 18:38:05 +0200

    dodano prosta misje

---
+++ RetroFPS/Assets/Deletus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deletus : MonoBehaviour {
+
+    public GameObject bulki;
+    public Mission mission;
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "Player")
+        {
+            mission.GetComponent<Mission>();
+            mission.NextObjective();
+            Destroy(bulki);
+       
+        }
+    }
+}
