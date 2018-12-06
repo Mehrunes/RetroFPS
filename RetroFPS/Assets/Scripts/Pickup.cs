e7e27bf2b7bdc2736edac36ec3dafcba341a6512
Krzcwi <krzcwi@st.amu.edu.pl>
2018-12-06 18:05:15 +0100

    Dodano podnoszony obiekt

---
+++ RetroFPS/Assets/Scripts/Pickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour,INteracts {
+    public bool picked = false;
+
+    public Transform Targetlocation;
+
+    // Update is called once per frame
+    void Update () {
+		if(picked == true)
+        {
+            this.transform.position = Targetlocation.position;
+        }
+	}
+
+    public void OnUse()
+    {
+        if (picked == false)
+        {
+            picked = true;
+        }
+        else {
+            picked = false;
+        }
+    }
+}
